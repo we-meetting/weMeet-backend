fc92afde958abe42ab53931c61cbbd03bf163aa9
최근원 <kidjustinchoi@gmail.com>
2024-01-01 19:21:28 +0900

    chore: initalize project

---
+++ src/app.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+
+@Injectable()
+export class AppService {
+  constructor(private prisma: PrismaService) {}
+
+  async getHello() {
+    const users = await this.prisma.user.findMany();
+    return `users: ${users.length}`;
+  }
+}
