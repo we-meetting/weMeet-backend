efee5ade435d680e24aa840e0818d17a1c4e103b
최근원 <kidjustinchoi@gmail.com>
2024-01-01 21:20:07 +0900

    feat: add user schema settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma.service';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env',
+      validationSchema: Joi.object({
+        DATABASE_URL: Joi.string().required(),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
