b43c77b35380e50ceeda8c715e47c2c2fd55de46
n ghtcjm <84369321+choi138@users.noreply.github.com>
2024-01-31 15:23:28 +0900

    fix: update main.ts

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: [process.env.NODE_ENV === 'prod' ? 'https://wemtmt.com/' : 'http://localhost:5173'],
+    origin: [process.env.NODE_ENV === 'prod' ? 'https://wemtmt.com' : 'http://localhost:5173'],
     credentials: true,
   });
 
